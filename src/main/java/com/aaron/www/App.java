f825cd5182bb78a331fff297ad425e9fc85a5e58
ahron <asteinbruch96@gmail.com>
2022-12-14 17:14:43 +0200

    test Expression Package

--- src/main/java/com/aaron/www/App.java
+++ src/main/java/com/aaron/www/App.java
@@ -1,5 +1,8 @@
 package com.aaron.www;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,8 @@ public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Expression expression = new ExpressionBuilder("3+2").build();
+        double result = expression.evaluate();
+        System.out.println(result);
     }
 }
